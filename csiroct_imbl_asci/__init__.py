2fcc8be6c039f9314fed51242a3b8bc5c4abc059
Darren Thompson <darren.thompson@csiro.au>
2019-08-15 16:26:11 +1000

    Added strided convert function

---
+++ csiroct_imbl_asci/__init__.py
@@ -0,0 +1,20 @@
+from .version import __version__
+from .convert import convert_epics_to_xtract_hdf5
+from .xtract import get_num_projections 
+from .xtract import assemble_command
+from .xtract import run_XLICTPreProc
+from .xtract import run_XLICTWorkflow
+from .xtract import run_XLICTRecon
+from .xtract import run_XLICOR
+
+# if somebody does "from csiroct-imbl-asci import *", this is what they will
+# be able to access:
+__all__ = [
+    'convert_epics_to_xtract_hdf5',
+    'get_num_projections',
+    'assemble_command',
+    'run_XLICTPreProc',
+    'run_XLICTWorkflow',
+    'run_XLICTRecon',
+    'run_XLICOR',
+]
---
+++ csiroct_imbl_asci/convert.py
@@ -0,0 +1,77 @@
+import os.path
+import h5py
+import logging
+import numpy
+
+import csiroct_imbl_asci.xtract as xtract
+
+file_imbl_h5_flat = 'BG_BEFORE.hdf'
+file_imbl_h5_dark = 'DF_BEFORE.hdf'
+file_imbl_h5_sample = 'SAMPLE.hdf'
+
+h5_dataset_epics = 'entry/data/data'
+
+def convert_epics_to_xtract_hdf5( file_flat, file_dark, file_sample, file_output, sample_start=0, sample_stride=1 ):
+    logging.debug('convert_epics_to_xtract_hdf5, sample_start = %d, sample_stride = %d', sample_start, sample_stride )
+    
+    # Open output X-TRACT HDF5 file
+    with h5py.File( file_output, 'w') as out_xtract:
+        logging.debug('Opened output - %s', file_output )
+        group_exchange = out_xtract.create_group( xtract.h5_group_xtract_exchange )
+
+        # Open input IMBL (EPICS) flat HDF5 file
+        with h5py.File( file_flat, 'r') as in_flat:
+            logging.debug('Opened flat - %s', file_flat )
+            # Copy dataset
+            in_flat.copy( h5_dataset_epics, group_exchange, 'data_flat' )
+            logging.debug('Copied flat dataset to - %s', 'data_flat' )
+
+        # Open input IMBL (EPICS) dark HDF5 file    
+        with h5py.File( file_dark, 'r') as in_dark:
+            logging.debug('Opened dark - %s', file_dark )
+            # Copy dataset
+            in_dark.copy( h5_dataset_epics, group_exchange, 'data_dark' )
+            logging.debug('Copied dark dataset to - %s', 'data_dark' )
+
+        # Open input IMBL (EPICS) sample HDF5 file    
+        with h5py.File( file_sample, 'r') as in_sample:
+            logging.debug('Opened sample - %s', file_sample )
+
+            if sample_start == 0 and sample_stride == 1:
+                # Simply copy the dataset as with darks and flats
+                in_sample.copy( h5_dataset_epics, group_exchange, 'data' )
+            else:
+                # Need to create a new dataset and copy strided projections
+                # from the given start index
+
+                dset_in = in_sample[h5_dataset_epics]
+                
+                # Determine the shape of the input sample dataset
+                shape = dset_in.shape
+                logging.debug('Sample input dataset shape - %s', str(shape) )
+
+                # Construct a list of indicies to copy
+                list_indexes = list( range( sample_start, shape[0], sample_stride ) )
+                logging.debug('list_indexes length = %d', len(list_indexes) )
+
+                # Create the output dataset
+                dset_out = group_exchange.create_dataset( 'data', (len(list_indexes), shape[1], shape[2]), dtype=dset_in.dtype )
+                logging.debug('Sample output dataset shape - %s', str(dset_out.shape) )
+
+                # Create a temporary array to hold a single projection
+                projection = numpy.zeros( ( 1, shape[1], shape[2] ), dtype=dset_out.dtype )
+
+                # Copy strided projections to the new dataset
+                index_output = 0
+                for index_input in list_indexes:
+                    # Read
+                    dset_in.read_direct( projection, source_sel=numpy.s_[index_input] )
+
+                    # Write
+                    dset_out.write_direct( projection, dest_sel=numpy.s_[index_output] )
+
+                    index_output += 1
+
+                logging.debug('copied %d sample projections', len(list_indexes) )
+            
+            return group_exchange['data'].shape[0]

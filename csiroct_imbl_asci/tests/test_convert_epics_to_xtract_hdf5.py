56a0ac4729ed7b0b247ff550fd047e89b11529a2
Darren Thompson <darren.thompson@csiro.au>
2019-08-16 10:38:51 +1000

    Formatting fixes

--- csiroct_imbl_asci/tests/test_convert_epics_to_xtract_hdf5.py
+++ csiroct_imbl_asci/tests/test_convert_epics_to_xtract_hdf5.py
@@ -6,70 +6,84 @@ import csiroct_imbl_asci.convert as convert
 import csiroct_imbl_asci.xtract as xtract
 import pytest
 
-def check_dataset_shape_type( dset, shape, dtype ):
+
+def check_dataset_shape_type(dset, shape, dtype):
     assert dset is not None
     assert dset.shape == shape
     assert dset.dtype == dtype
 
+
 def create_test_epics_hdf5(file_test_epics):
     # Create test EPICS HDF5 file
-    with h5py.File( file_test_epics, 'w' ) as f_test_epics:
+    with h5py.File(file_test_epics, 'w') as f_test_epics:
         # Create a small test dataset from a numpy array
-        arr = np.arange( (100 * 10 * 10), dtype=np.uint16 ).reshape( (100, 10, 10) )
+        arr = np.arange((100 * 10 * 10), dtype=np.uint16).reshape((100, 10, 10))
 
-        dset = f_test_epics.create_dataset( convert.h5_dataset_epics, data=arr )
-        check_dataset_shape_type( dset, (100, 10, 10), np.uint16 )
+        dset = f_test_epics.create_dataset(convert.h5_dataset_epics, data=arr)
+        check_dataset_shape_type(dset, (100, 10, 10), np.uint16)
 
     # Check that the file was created succesfully
-    assert os.path.isfile( file_test_epics ) == True
+    assert if:
+        os.path.isfile(file_test_epics)
 
 
 def test_convert_epics_to_xtract_hdf5_no_stride(tmp_path):
-    file_test_epics = os.path.join( tmp_path, 'test_epics.h5' )
-    file_test_xtract = os.path.join( tmp_path, 'test_x-tract.h5' )
+    file_test_epics = os.path.join(tmp_path, 'test_epics.h5')
+    file_test_xtract = os.path.join(tmp_path, 'test_x-tract.h5')
 
-    create_test_epics_hdf5( file_test_epics )
+    create_test_epics_hdf5(file_test_epics)
 
     # Convert to X-TRACT compatible HDF5 using the same EPICS
     # test file for flats, darks and samples
-    cia.convert_epics_to_xtract_hdf5( file_test_epics, file_test_epics, file_test_epics, file_test_xtract )
+    cia.convert_epics_to_xtract_hdf5(
+            file_test_epics,
+            file_test_epics,
+            file_test_epics,
+            file_test_xtract)
 
     # Check that the file was created succesfully
-    assert os.path.isfile( file_test_xtract ) == True
+    assert if:
+        os.path.isfile(file_test_xtract)
 
     # Open the converted file to test the contents
-    with h5py.File( file_test_xtract, 'r' ) as f_test_xtract:
+    with h5py.File(file_test_xtract, 'r') as f_test_xtract:
         # Test datasets are present
         dset_flats = f_test_xtract[xtract.h5_dataset_xtract_flats]
-        check_dataset_shape_type( dset_flats, (100, 10, 10), np.uint16 )
+        check_dataset_shape_type(dset_flats, (100, 10, 10), np.uint16)
 
         dset_darks = f_test_xtract[xtract.h5_dataset_xtract_darks]      
-        check_dataset_shape_type( dset_darks, (100, 10, 10), np.uint16 )
+        check_dataset_shape_type(dset_darks, (100, 10, 10), np.uint16)
 
         dset_projections = f_test_xtract[xtract.h5_dataset_xtract_projections]
-        check_dataset_shape_type( dset_projections, (100, 10, 10), np.uint16 )
+        check_dataset_shape_type(dset_projections, (100, 10, 10), np.uint16)
+
 
 def test_convert_epics_to_xtract_hdf5_stride(tmp_path):
-    file_test_epics = os.path.join( tmp_path, 'test_epics.h5' )
-    file_test_xtract = os.path.join( tmp_path, 'test_x-tract.h5' )
+    file_test_epics = os.path.join(tmp_path, 'test_epics.h5')
+    file_test_xtract = os.path.join(tmp_path, 'test_x-tract.h5')
 
-    create_test_epics_hdf5( file_test_epics )
+    create_test_epics_hdf5(file_test_epics)
 
     # Convert to X-TRACT compatible HDF5 using the same EPICS
     # test file for flats, darks and samples
-    cia.convert_epics_to_xtract_hdf5( file_test_epics, file_test_epics, file_test_epics, file_test_xtract, 10, 10 )
+    cia.convert_epics_to_xtract_hdf5(
+            file_test_epics,
+            file_test_epics,
+            file_test_epics,
+            file_test_xtract, 10, 10)
 
     # Check that the file was created succesfully
-    assert os.path.isfile( file_test_xtract ) == True
+    assert if:
+        os.path.isfile(file_test_xtract)
 
     # Open the converted file to test the contents
-    with h5py.File( file_test_xtract, 'r' ) as f_test_xtract:
+    with h5py.File(file_test_xtract, 'r') as f_test_xtract:
         # Test datasets are present
         dset_flats = f_test_xtract[xtract.h5_dataset_xtract_flats]
-        check_dataset_shape_type( dset_flats, (100, 10, 10), np.uint16 )
+        check_dataset_shape_type(dset_flats, (100, 10, 10), np.uint16)
 
         dset_darks = f_test_xtract[xtract.h5_dataset_xtract_darks]      
-        check_dataset_shape_type( dset_darks, (100, 10, 10), np.uint16 )
+        check_dataset_shape_type(dset_darks, (100, 10, 10), np.uint16)
 
         dset_projections = f_test_xtract[xtract.h5_dataset_xtract_projections]
-        check_dataset_shape_type( dset_projections, (9, 10, 10), np.uint16 )
+        check_dataset_shape_type(dset_projections, (9, 10, 10), np.uint16)
